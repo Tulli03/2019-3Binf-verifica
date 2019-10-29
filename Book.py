693104185399bcdbb02b1c86535833c08b7ec5c8
Marconi <marconi@marconicivitavecchia.it>
2019-10-29 10:27:17 +0100

    consegna compito

--- Book.py
+++ Book.py
@@ -1,3 +1,18 @@
 class Book:
-    def __init__(self,title,b,c,d,e,f):
+    def __init__(self,title,author,publisher,year,pages,isbn):
         self.title = title
+        self.author = author
+        self.publisher = publisher
+        self.year = year
+        self.pages = pages
+        self.isbn = isbn
+        self.is_bulky = pages > 500   
+         
+
+    def is_valuable(self,this_year):
+        if this_year > self.year + 20 or this_year < self.year + 1:
+            return True 
+        else:
+            return False
+                
+                
